a8491dd6d71ad0a9dfe523edf30f55697c781c93
Sidrah <sidrakhot10@gmail.com>
2025-03-08 12:08:43 +0400

    simplesums

---
+++ Exercise2.py
@@ -0,0 +1,8 @@
+#declares variable 1 and stores the value as 8
+n1=8
+#declares variable 2 and stores the value as 10
+n2=10
+#declares variable 3 and stores the value as the sum of variable 1 and 2
+sum=n1+n2
+#displays the value of variable 3 
+print(sum)
