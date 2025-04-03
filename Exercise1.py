dddf01c10b011dec232d02750d9ee5199b5eb236
Sidrah <sidrakhot10@gmail.com>
2025-04-03 15:24:49 +0400

    Update Exercise1.py

---
+++ Exercise1.py
@@ -0,0 +1,8 @@
+#assigning the first variable
+word1='Coding'
+#assigning the second variable
+word2='Is'
+#assigning the third variable
+word3='Cool'
+#using string concatenation to combine the variables and printing the phrase
+print(word1+' '+word2+' '+word3)
