778f765650c4aebb16eca2785b6a2bbcdc1a01d6
Sidrah <sidrakhot10@gmail.com>
2025-04-05 13:17:42 +0400

    Create Exercise4.py
    CapitalsQuiz

---
+++ Exercise4.py
@@ -0,0 +1,85 @@
+# printing the question number
+print('question 1')
+guess1=input('what is the capital of France?') #stores the user's answer as the value
+
+# setting the conditions of executing 2 different outputs
+if guess1.lower()=='paris': #checks if the user's answer is correct by converting the string to lowercase
+    print('your answer is right, you are doing great!') #this will be executed if user is right
+else:
+    # this statement will be executed if the user was wrong
+    print('you are wrong, the correct answer is Paris')
+
+# printing the question number
+print('question 2')
+#asking the question
+guess2=input('what is the capital of Belgium?')
+# checking if user's answer is right
+if guess2.lower()=='brussels':
+    print('your answer is right, youre doing great!') #executes this statment if user is right
+else:
+    print('you are wrong, the correct answer is Brussels')#executes this statment if user is wrong
+
+#printing the next queation number and question
+print('question 3')
+guess3=input('what is the capital of Georgia?')
+if guess3.lower()=='tbilisi':
+    print('your answer is right, you are doing great!') #executed if user is right
+else:
+    print('you are wrong, the correct answer is Tbilisi') #executed if user is wrong
+
+#printing the next queation number and question
+print('question 4')
+guess4=input('what is the capital of Austria?')
+if guess4.lower()=='vienna':
+    print('your answer is right, you are doing great!') #executed if user is right
+else:
+    print('you are wrong, the correct answer is Vienna')#executed if user is wrong
+
+#printing the next queation number and question
+print('question 5')
+guess5=input('what is the capital of italy?')
+if guess5.lower()=='rome':
+    print('your answer is right, you are doing great!') #executed if user is right
+else:
+    print('you are wrong, the correct answer is Rome') #executed if user is wrong
+
+#printing the next queation number and question
+print('question 6')
+guess6=input('what is the capital of Czechia?')
+if guess6.lower()=='prague':
+    print('your answer is right, you are doing great!') #executed if user is right
+else:
+    print('you are wrong, the correct answer is Prague') #executed if user is wrong
+
+#printing the next queation number and question
+print('question 7')
+guess7=input('what is the capital of germany?')
+if guess7.lower()=='berlin':
+    print('your answer is right, you are doing great!') #executed if user is right
+else:
+    print('you are wrong, the correct answer is Berlin') #executed if user is wrong
+
+#printing the next queation number and question
+print('question 8')
+guess8=input('what is the capital of finland?')
+if guess8.lower()=='helsinki':
+    print('your answer is right, you are doing great!') #executed if user is right
+else:
+    print('you are wrong, the correct answer is Helsinki') #executed if user is wrong
+
+#printing the next queation number and question
+print('question 9')
+guess9=input('what is the capital of ireland?')
+if guess9.lower()=='dublin':
+    print('your answer is right, you are doing great!') #executed if user is right
+else:
+    print('you are wrong, the correct answer is Dublin') #executed if user is wrong
+
+#printing the next queation number and question
+print('question 10')
+guess1=input('what is the capital of the UK?')
+if guess1.lower()=='london':
+    print('your answer is right, you are doing great!') #executed if user is right
+else:
+    print('you are wrong, the correct answer is london') #executed if user is wrong
+    
